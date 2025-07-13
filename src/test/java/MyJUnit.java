e06f6014869b964e496598f31e53c67b7772c08e
provashish95 <roytonmoy95@gmail.com>
2025-07-13 12:41:10 +0600

    Open websites home page

--- src/test/java/MyJUnit.java
+++ src/test/java/MyJUnit.java
@@ -1,12 +1,31 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 public class MyJUnit {
-    public void setup() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/");
-        String title = driver.getTitle();
 
-        System.out.println(title);
+    WebDriver driver;
+    @BeforeAll
+
+    public void setup() {
+        driver = new ChromeDriver();
+
+        //see in maximum screen...
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+    }
+    @Test
+    public void visitSite(){
+        driver.get("https://demoqa.com/");
+    }
+
+    public void teardown(){
+        driver.quit();
     }
 }
